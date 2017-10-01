bfcbd98a0c1ee5d0012828c04e16231c3bb84e4e
Lance <lance.labrague@gmail.com>
2017-10-01 12:27:09 +0800

    Completed file reading, added FCFS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 var canvas = document.getElementById("viewport");
 var context = canvas.getContext("2d");
 
+//Array initialization for jobs
+var jobs = [];
+
+function readByLine(arr){
+    for(i = 1; i < arr.length; i++){
+        line = arr[i].split(/\t\t\t|\t\t|\t/);
+        if(line.length < 4)
+            continue;
+        else
+            jobs.push(new Job(parseInt(line[0]), parseInt(line[1]), parseInt(line[2]), parseInt(line[3])));
+    }
+}
+
 function readSingleFile(evt) {
     //Retrieve the first (and only!) File from the FileList object
     var f = evt.target.files[0];
@@ -8,47 +21,46 @@ function readSingleFile(evt) {
     if (f) {
         var r = new FileReader();
         r.onload = function(e) {
-        var contents = e.target.result;
-        // alert( "Got the file.n"
-        //       +"name: " + f.name + "n"
-        //       +"type: " + f.type + "n"
-        //       +"size: " + f.size + " bytesn"
-        //       + "starts with: " + contents.substr(1, contents.indexOf("n"))
-        // );
-        console.log(contents);
+            var contents = e.target.result;
+            var arr = contents.split("\n");
+            readByLine(arr);
         }
         r.readAsText(f);
     } else {
         alert("Failed to load file");
     }
 }
-document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
 
+document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
+document.getElementById('FCFS').addEventListener('click', FCFS, false);
+// document.getElementById('SJF').addEventListener('click', SJF, false);
+// document.getElementById('SRPT').addEventListener('click', SRPT, false);
+// document.getElementById('PRIORITY').addEventListener('click', PRIORITY, false);
+// document.getElementById('ROUND-ROBIN').addEventListener('click', ROUNDROBIN, false);
 
 function Job(id, arrival, burstTime, priority){
+    console.log("JOB: ", id, arrival, burstTime, priority);
     this.id = id;
     this.arrival = arrival;
-    this.burstTime = bustTime;
+    this.burstTime = burstTime;
     this.priority = priority;
 
     this.waitingTime = 0;
     this.computingTime = 0;
 }
 
-//Array initialization for jobs
-var jobs = [];
 
 function FCFS(){
+    console.log("FCFS");
     var waitingTime = 0;
-    for(var job of jobs){
-        job.waitingTime = waitingTime;
-        job.computingTime = job.waitingTime + job.burstTime;
 
-        waitingTime += job.burstTime;
+    for(j = 0; j < jobs.length; j++){
+        jobs[j].waitingTime = waitingTime;
+        jobs[j].computingTime = jobs[j].waitingTime + jobs[j].burstTime;
+        waitingTime += jobs[j].burstTime;
     }
-}
 
-function main(){
+    renderUI();
 }
 
 function renderUI(){
@@ -60,12 +72,18 @@ function renderUI(){
     context.fillText("Jobs - " + jobs.length, 10, 40);
 
     context.font = "15px Arial";
+
+    context.fillText("JOB", 10, 80);
+    context.fillText("WAITING TIME", 160, 80);
+    context.fillText("COMPUTING TIME", 310, 80);
+
+
     for(p = 0; p < jobs.length; p++){
 
         context.fillStyle = "#00AAFF";
 
-        context.fillText("Job " + jobs[p].id, 10, 80+(p*20));
+        context.fillText("Job " + jobs[p].id, 10, 100+(p*20));
+        context.fillText(jobs[p].waitingTime, 160, 100+(p*20));
+        context.fillText(jobs[p].computingTime, 310, 100+(p*20));
     }
 }
-
-setInterval(main,1000);
